427142438ce9987d5d8179c0c0f37a5cd86cc182
Yermi <yermi72@gmail.com>
2026-05-13 09:57:09 +0000

    add numeric enum custom field plugin with input component and documentation

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,11 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+	'numeric-enum': {
+		enabled: true,
+		resolve: './src/plugins/numeric-enum',
+		config: {},
+	},
+});
 
 export default config;
